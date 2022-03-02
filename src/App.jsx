07b1145613fe780d80d25d7c324d14908f211bf4
Irissf <iris.seijo@gmail.com>
2022-03-02 11:38:39 +0100

    Desde cero

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//Podemos poner js o jsx, autocompleta mejor con jsx
+import React from 'react'
+
+const App = () => {
+
+    const nombre = "App"
+
+    return (
+        //un jsx ()javascript xml
+        <div>
+            <h1>Componente {nombre}</h1> 
+        </div>
+    )
+}
+
+export default App
