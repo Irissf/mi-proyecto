34de14d7a0c92571f41f30a50909fa020023a38f
Irissf <iris.seijo@gmail.com>
2022-03-10 11:56:56 +0100

    Submit

--- src/components/FormNoControlado.jsx
+++ src/components/FormNoControlado.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const FormNoControlado = () => {
+
+  //Hook para las referencias
+  const formulario = useRef (null);
+
+  const handleSubmit = (e) => {
+      //evitar que se recarge la pagina al mandar el formulario
+      e.preventDefault();
+      console.log(e);
+  }
+
+
   return (
     <div>
         <h2>Formularios no Controlados</h2>
-        <form>
-            <input type="text" placeholder='Ingrese ToDo' name="todoName"></input>
-            <textarea name='todoDescripcion' placeholder='Descripción'></textarea>
-            <select name='todoEstado' className='select'>
+        {/* Evitaremos los getElementByID o los QuerySelector y usaremos
+        en las contadas ocasiones que las necesitemos, las referencias */}
+        <form ref={formulario} onSubmit={handleSubmit}>
+            <input type="text" placeholder='Ingrese ToDo' name="todoName" defaultValue="tarea 1"></input>
+            <textarea name='todoDescripcion' placeholder='Descripción' defaultValue="descripción tarea 1"></textarea>
+            <select name='todoEstado' className='select' defaultValue="pendiente">
                 <option value="pendiente">Pendiente</option>
                 <option value="completada">Completada</option>
             </select>
